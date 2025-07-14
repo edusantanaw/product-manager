dd4c99e49427ac11e899e1e7b89624c37dea7570
edusantanaw <eduardosantana20122002@gmail.com>
2025-07-14 19:41:54 -0300

    chore: configuração inicial do frontend

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,10 +4,16 @@ import { ValidationPipe } from '@nestjs/common';
 import { ExceptionFilterImpl } from './config/filters/exception.filter';
 
 async function bootstrap() {
+  const frontedURL = process.env.FRONTEND_URL;
+  const port = process.env.PORT;
+
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: [frontedURL],
+    credentials: true,
+  });
   app.useGlobalFilters(new ExceptionFilterImpl());
   app.useGlobalPipes(new ValidationPipe());
-  const port = process.env.PORT;
   await app.listen(Number(port ?? 3000));
 }
 void bootstrap();
